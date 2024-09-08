0aab76515ce9d0cc4aa341eb973f9dc5a5cff5aa
Mauricio <mauriciodiego2019@gmail.com>
2024-09-08 18:48:42 -0500

    a

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 from mcr.microsoft.com/dotnet/sdk:8.0
 workdir app
-run dotnet new blazor -o BlazingPizzaSite
+#run dotnet new blazor -o BlazingPizzaSite
+run mkdir BlazingPizzaSite
 workdir BlazingPizzaSite
-copy . .
+copy PizzaApp/* .
 run dotnet restore
 run dotnet build
 entrypoint dotnet run --urls="http://0.0.0.0:8000"
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/chupino/lab5daea2.git tuto
 cd tuto
+git clone https://github.com/MicrosoftDocs/mslearn-interact-with-data-blazor-web-apps.git PizzaApp
 
 docker build -t tuto .
 
@@ -10,4 +11,5 @@ else
     exit 1
 fi
 
-docker run -itp 8000:8000 tuto
+docker run -dp 8000:8000 tuto
+docker exec -it $(docker ps -lq) sh
