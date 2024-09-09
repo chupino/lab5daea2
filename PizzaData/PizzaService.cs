a096dd71643f181b9f62e9635f2f72e8b9768dda
Mauricio <mauriciodiego2019@gmail.com>
2024-09-08 19:50:53 -0500

    a

---
+++ PizzaData/PizzaService.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+
+namespace BlazingPizza.PizzaData
+{
+    public class PizzaService
+    {
+        public Task<Pizza[]> GetPizzasAsync()
+        {
+        // Call your data access technology here
+        }
+    }
+}
