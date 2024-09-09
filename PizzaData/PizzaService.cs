8152b03c467d8972db6d97f1b5a133a188c27d66
Mauricio <mauriciodiego2019@gmail.com>
2024-09-08 20:07:57 -0500

    a

--- PizzaData/PizzaService.cs
+++ PizzaData/PizzaService.cs
@@ -6,11 +6,11 @@ namespace BlazingPizza.PizzaData
         {
             var pizzas = new[]
             {
-                new Pizza { PizzaId = 1, Name = "Margherita", Description = "Tomato, mozzarella, basil", Price = 9.99m, Vegetarian = true, Vegan = false },
-                new Pizza { PizzaId = 2, Name = "Pepperoni", Description = "Tomato, mozzarella, pepperoni", Price = 12.99m, Vegetarian = false, Vegan = false },
-                new Pizza { PizzaId = 3, Name = "Veggie", Description = "Tomato, mozzarella, peppers, onions, mushrooms", Price = 11.99m, Vegetarian = true, Vegan = true },
-                new Pizza { PizzaId = 4, Name = "BBQ Chicken", Description = "BBQ sauce, mozzarella, chicken", Price = 13.99m, Vegetarian = false, Vegan = false },
-                new Pizza { PizzaId = 5, Name = "Four Cheese", Description = "Mozzarella, cheddar, parmesan, gorgonzola", Price = 14.99m, Vegetarian = true, Vegan = false }
+                new Pizza { Name = "Margherita", Description = "Tomato, mozzarella, basil", Price = 9.99m, Vegetarian = true, Vegan = false },
+                new Pizza { Name = "Pepperoni", Description = "Tomato, mozzarella, pepperoni", Price = 12.99m, Vegetarian = false, Vegan = false },
+                new Pizza { Name = "Veggie", Description = "Tomato, mozzarella, peppers, onions, mushrooms", Price = 11.99m, Vegetarian = true, Vegan = true },
+                new Pizza { Name = "BBQ Chicken", Description = "BBQ sauce, mozzarella, chicken", Price = 13.99m, Vegetarian = false, Vegan = false },
+                new Pizza { Name = "Four Cheese", Description = "Mozzarella, cheddar, parmesan, gorgonzola", Price = 14.99m, Vegetarian = true, Vegan = false }
             };
 
             return Task.FromResult(pizzas);
