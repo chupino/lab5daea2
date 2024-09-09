b281a46f3bab10754620ccedf27ce829135bdc9e
Mauricio <mauriciodiego2019@gmail.com>
2024-09-08 20:06:41 -0500

    a

--- PizzaData/Pizza.cs
+++ PizzaData/Pizza.cs
@@ -1,12 +1,11 @@
 namespace BlazingPizza.PizzaData
 {
     public class Pizza
-    {
-        public int PizzaId { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public bool Vegetarian { get; set; }
-        public bool Vegan { get; set; }
-    }
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool Vegetarian { get; set; }
+    public bool Vegan { get; set; }
+    public decimal Price { get; set; }
+}
 }
